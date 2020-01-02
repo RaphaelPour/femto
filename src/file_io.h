c4988db138cee3ada595a577ab4c36630049e03f
Raphael Pour <info@raphaelpour.de>
2020-01-02 20:16:04 +0100

    FileIO: Add missing header includes

--- src/file_io.h
+++ src/file_io.h
@@ -1,6 +1,9 @@
 #ifndef FILE_IO_H
 #define FILE_IO_H
 
+#include <stdlib.h>
+#include <stdio.h>
+
 size_t fe_file_load(char *filename, char **content);
 void fe_file_save(char *filename, char *content, size_t length);
 
