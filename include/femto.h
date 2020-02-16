d485a4dee845d2727be2c825c826c2614630fc38
Raphael Pour <info@raphaelpour.de>
2020-02-16 19:32:08 +0100

    Main: Add I/O-Loop including command interpreter Main: Init logger

--- include/femto.h
+++ include/femto.h
@@ -3,9 +3,11 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 #include <file_io.h>
 #include <terminal.h>
 #include <screen.h>
-
+#include <navigation.h>
+#include <helper.h>
 #endif // FEMTO_H
