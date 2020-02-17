534815d31667a3358dd2eff6862be40189fb446f
Raphael Pour <info@raphaelpour.de>
2020-02-17 22:02:25 +0100

    Testing: Restructures unit tests Testing: Add util tests

---
+++ test/buffer_test.c
@@ -0,0 +1,70 @@
+#include "buffer_test.h"
+
+void test_create_buffer()
+{
+    TEST_IT_NAME("creates a buffer");
+
+    Buffer *buf = fe_create_buffer();
+    assert(buf && "Buffer is NULL");
+
+    fe_free_buffer(buf);
+    assert(!buf->data && "Buffer is not freed correctly");
+    TEST_OK
+}
+
+void test_append_to_buffer()
+{
+    TEST_IT_NAME("appends to a buffer");
+    TEST_SKIP;
+    const char *test_string = "const char *test_string";
+    Buffer *buf = fe_create_buffer();
+    assert(buf && "Buffer is NULL");
+
+    fe_append_to_buffer(buf, test_string, strlen(test_string));
+    
+    assert( strcmp(buf->data, test_string) == 0 && "Strings differ");
+    fe_free_buffer(buf);
+    assert(buf->data == NULL && "Buffer is not freed correctly");
+
+    TEST_OK
+}
+
+void test_append_to_buffer_multiple_times()
+{
+    TEST_IT_NAME("appends to a buffer multiple times");
+    TEST_SKIP;
+    char *test_strings[] = 
+    {
+        "A common mistake that people make",
+        "when trying to design something ",
+        "completely foolproof is to underestimate",
+        "the ingenuity of complete fools."
+    };
+    
+    Buffer *buf = fe_create_buffer();
+    assert(buf && "Buffer is NULL");
+
+    int i;
+    for(i=0;i<4;i++)
+        fe_append_to_buffer(buf, test_strings[i], strlen(test_strings[i]));
+    
+    for(i=0;i<4;i++)
+        if(expect_s_included(buf->data, test_strings[i], "String missing in buffer") == FAIL)
+        {
+            TEST_FATALITY
+            exit(EXIT_FAILURE);
+        }
+
+    fe_free_buffer(buf);
+    assert(!buf->data && "Buffer is not freed correctly");
+
+    TEST_OK
+}
+void test_suite_buffer()
+{
+    TEST_SUITE_NAME("Buffer");
+
+    test_create_buffer();
+    test_append_to_buffer();
+    test_append_to_buffer_multiple_times();
+}
---
+++ test/fileio_test.h
@@ -0,0 +1,14 @@
+#ifndef FILEIO_TEST_H
+#define FILEIO_TEST_H
+
+#include "test_util.h"
+#include <session.h>
+#include <buffer.h>
+#include <file_io.h>
+
+void test_load_file();
+void test_load_file_with_long_lines();
+void test_save_file();
+void test_suite_file_io();
+
+#endif 
