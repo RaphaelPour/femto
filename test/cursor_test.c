74d12a257eb2ba89e31370e028db6d15f9d89b6a
Raphael Pour <info@raphaelpour.de>
2020-03-10 22:09:16 +0100

    Testing: Add session helper for cursor tests

--- test/cursor_test.c
+++ test/cursor_test.c
@@ -9,10 +9,58 @@ static Session* create_session_zero_initialized()
     s->cursor_position = (TerminalPosition){{1},{1}};
     s->offset = (TerminalPosition){{0},{0}};
     s->line_count = 0;
+    s->content_length = 0;
     s->lines = 0;
     s->edit_mode = 0;
 
     return s;
+}
+
+static Session* create_session_perfect_fit_by_input(int line_count, char *lines[])
+{
+    Session *s = create_session_zero_initialized();
+
+    s->lines = calloc(line_count, sizeof(Line*));
+
+    int i, total_length = 0, longest_line_length = 0;
+    for(i=0;i<line_count;i++)
+    {
+        int len = strlen(lines[i]);
+        s->lines[i].content = (char*)malloc(len);
+        memcpy(s->lines[i].content, lines[i], len);
+
+        s->lines[i].index = i;
+        s->lines[i].length = len;
+
+        total_length += len;
+        longest_line_length = MAX(longest_line_length, len);
+    }
+
+    s->line_count = line_count;
+    s->content_length = total_length;
+
+    s->terminal_size.height = line_count;
+    s->terminal_size.width = longest_line_length;
+
+    return s;
+}
+
+static void clean_up_session(Session *s)
+{
+    if(s->filename) free(s->filename);
+
+    /*
+    if(s->lines)
+    {   
+        int i;
+        for(i=0;i<s->line_count;i++)
+            if(s->lines[i].content)
+                free(s->lines[i].content);
+        
+        free(s->lines);
+    }*/
+
+    free(s);
 }
 
 static Session* duplicate_session(Session *s)
