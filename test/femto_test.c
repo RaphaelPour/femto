5c4cccddd4f9aa6ccafd3619e7512d63e8438cd8
Raphael Pour <info@raphaelpour.de>
2020-03-21 23:42:33 +0100

    Build: Add Build date and version

--- test/femto_test.c
+++ test/femto_test.c
@@ -5,10 +5,25 @@
 #include <unistd.h>
 #include <sys/ioctl.h>
 
+#include <helper.h>
 #include "femto_test.h"
+
+#ifndef BUILD_DATE
+#define BUILD_DATE "fix your makefile"
+#endif
+
+#ifndef BUILD_VERSION
+#define BUILD_VERSION "fix your makefile"
+#endif
 
 int main(int argc, char *argv[])
 {
+    printf("BuildDate: %s\n", BUILD_DATE);
+    printf("BuildVersion: %s\n", BUILD_VERSION);
+
+
+    lopen("femto_test.log", LOG_DEBUG);
+
     test_suite_session();
     test_suite_terminal();
     test_suite_buffer();
