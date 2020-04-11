b59c03ce66865f5abac64a33f68202d4c8ac89da
Raphael Pour <info@raphaelpour.de>
2020-04-11 18:56:36 +0200

    Main: Refactoring, improve readability

--- source/femto.c
+++ source/femto.c
@@ -19,24 +19,24 @@ int main(int argc, char *argv[])
 {
 
     /* Argument parsing */
-    if(argc != 2)
+    if( argc != 2 )
     {
-        printf("usage: femto <file> | -v\n");
+        printf( "usage: femto <file> | -v\n" );
         return EXIT_FAILURE;
     }
 
-    if(strcmp(argv[1],"-v") == 0)
+    if( strcmp( argv[1], "-v" ) == 0 )
     {
-        printf("BuildDate: %s\n", BUILD_DATE);
-        printf("BuildVersion: %s\n", BUILD_VERSION);
+        printf( "BuildDate: %s\n", BUILD_DATE );
+        printf( "BuildVersion: %s\n", BUILD_VERSION );
         return EXIT_SUCCESS;
     }
 
-    lopen("femto.log", LOG_DEBUG);
+    lopen( "femto.log", LOG_DEBUG );
     char *filename = argv[1];
 
     /* Create session with delivered file */
-    Session *session= fe_init_session(filename);
+    Session *session= fe_init_session( filename );
 
     /* 
      * Set terminal mode to raw in order to disable 
@@ -49,46 +49,46 @@ int main(int argc, char *argv[])
      */
     int c;
     int exit_femto = 0;
-    while( !exit_femto )
+    while( ! exit_femto )
     {
         /* Render the current buffer to the terminal screen*/
-        fe_refresh_screen(session);
+        fe_refresh_screen( session );
 
         c = fe_get_user_input();
-        lprintf(LOG_INFO, "read user input '%c' (%d)", c,c);
+        lprintf( LOG_INFO, "read user input '%c' (%d)", c, c );
 
-        switch(c)
+        switch( c )
         {
             case UP:
-                fe_move(session,0,-1);
+                fe_move( session, 0, -1);
                 break;
             case LEFT:
-                fe_move(session,-1,0);
+                fe_move( session, -1, 0);
                 break;
             case DOWN:
-                fe_move(session,0,1);
+                fe_move( session, 0, 1);
                 break;
             case RIGHT:
-                fe_move(session,1,0);
+                fe_move( session, 1, 0);
                 break;
             case ESCAPE:
                 exit_femto = 1;
-                lprintf(LOG_INFO, "exited by user");
+                lprintf( LOG_INFO, "exited by user" );
                 break;
             case ENTER_MAC:
             case ENTER:
-                fe_insert_line(session);
-                fe_move(session,0,1);
+                fe_insert_line( session );
+                fe_move( session, 0, 1);
                 break;
             case BACKSPACE:
-                fe_remove_char_at_cursor(session);
-                fe_move(session,-1,0);
+                fe_remove_char_at_cursor( session );
+                fe_move( session, -1, 0 );
                 break;
             default:
-                if(isprint(c))
+                if( isprint( c ) )
                 {
-                    fe_insert_char(session, c);
-                    fe_move(session,1,0);
+                    fe_insert_char( session, c );
+                    fe_move( session, 1, 0);
                 }
         }
 
@@ -98,7 +98,7 @@ int main(int argc, char *argv[])
     fe_disable_raw_mode();
 
     /* Free resources aka make valgrind happy */
-    fe_free_session(session);
+    fe_free_session( session );
 
     lclose();
     return EXIT_SUCCESS;
