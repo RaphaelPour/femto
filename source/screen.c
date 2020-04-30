d9917f0646e2d10ce5250375f0851ce1f8e4c96e
Raphael Pour <info@raphaelpour.de>
2020-04-30 09:40:51 +0200

    Logging: Improve log message readability

--- source/screen.c
+++ source/screen.c
@@ -19,7 +19,7 @@ char* fe_create_set_cursor_command(Session *s)
              s->cursor_position.x);
 
 
-    lprintf(LOG_INFO, "set cursor command: '%x', pos: %3d/%3d", buf, s->cursor_position.x, s->cursor_position.y);
+    lprintf(LOG_INFO, "set cursor command: x=%d y=%d", s->cursor_position.x, s->cursor_position.y);
 
     return buf;
 }
