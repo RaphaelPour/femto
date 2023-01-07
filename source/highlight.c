6c52bbd527daf85282610ace3452c11a36763a94
Tch1b0 <jpour3006@gmail.com>
2023-01-07 11:19:59 +0100

    add highlighting functionality

--- source/highlight.c
+++ source/highlight.c
@@ -35,11 +35,10 @@ static int str_empty( char *str ) {
     return str[0] == '\0';
 }
 
-// ! Does not work as intended
 static Buffer *apply_color( Highlighter *h, Buffer *buf_text ) {
     char *str_text = malloc(sizeof( char ) * ( buf_text->length + 1 ));
     memcpy(str_text, buf_text->data, buf_text->length);
-    str_text[buf_text->length - 1] = '\0';
+    str_text[buf_text->length] = '\0';
 
     Buffer *new_text = fe_create_buffer();
 
@@ -48,17 +47,22 @@ static Buffer *apply_color( Highlighter *h, Buffer *buf_text ) {
         HighlightExpression he = h->expressions[i];
         int err = regexec( &he.expression, str_text, 0, NULL, 0 );
 
-        if ( strcmp(str_text, "void") ) {
-            char *ansii_end = "\e[m";
-            int ansii_end_len = 6;
+        if ( ! err )
+        {
+            char *ansii_end = "\E[0m";
+            int ansii_end_len = 5;
 
             fe_append_to_buffer( new_text, he.color, strlen(he.color) ); 
             fe_append_to_buffer( new_text, buf_text->data, buf_text->length );
             fe_append_to_buffer( new_text, ansii_end, ansii_end_len );
 
-            lprintf(LOG_INFO, "apply_color -> %*.d" , new_text->length, new_text->data);
-
             return new_text;
+        } 
+        else if ( err != REG_NOMATCH )
+        {
+            char msgbuff[100];
+            regerror(err, &he.expression, msgbuff, 100);
+            lprintf( LOG_ERROR, msgbuff );
         }
     }
 
@@ -69,10 +73,8 @@ static Buffer *apply_color( Highlighter *h, Buffer *buf_text ) {
 static HighlightExpression *fe_init_highlight_expression( char *str_expr, char *color ) {
     HighlightExpression *he = (HighlightExpression*) malloc( sizeof( HighlightExpression ));
 
-    regex_t comp_expr;
-    regcomp( &comp_expr, str_expr, 0 );
 
-    he->expression = comp_expr;
+    regcomp( &he->expression, str_expr, REG_EXTENDED );
     he->color = color;
 
     return he;
@@ -85,8 +87,12 @@ Highlighter *fe_init_highlighter( const char *filename ) {
 
     if( strcmp(h->filetype, "c") || strcmp(h->filetype, "h") ) {
         h->expressions = malloc( sizeof( HighlightExpression ) * 5 );
-        h->expressions[0] = *fe_init_highlight_expression( "void|static", "\e[0;31" );
-        h->expressions_len = 1;
+        h->expressions[0] = *fe_init_highlight_expression( "^(void|struct|int|char|return|if|for|while|do|else|const)$", "\E[0;31m" );
+        h->expressions[1] = *fe_init_highlight_expression( "^[0-9]{1,}$", "\E[0;34m" );
+        h->expressions[2] = *fe_init_highlight_expression( "^[a-zA-Z0-9]{1,}\\(", "\E[0;35m" );
+        h->expressions[3] = *fe_init_highlight_expression( "^[A-Z][a-zA-Z]*$", "\E[0;33m" );
+        h->expressions[4] = *fe_init_highlight_expression( "^(\\+)$", "\E[0;33m" );
+        h->expressions_len = 5;
     } else {
         h->expressions_len = 0;
     }
@@ -106,7 +112,7 @@ Buffer *fe_highlight(Highlighter *h, Buffer *text) {
     for( int i = 0; i <= text->length; i++ ) {
         char c = i < text->length ? text->data[i] : ' ';
         
-        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') )
+        if( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || c == '(' || c == '_' )
         {
             fe_append_to_buffer( buf, &c, 1 );
         } 
@@ -127,4 +133,10 @@ Buffer *fe_highlight(Highlighter *h, Buffer *text) {
 
     fe_free_buffer(buf);
     return new_text;
+}
+
+void fe_free_highlighter(Highlighter *h) {
+    free( h->filetype );
+    free( h->expressions );
+    free( h );
 }
